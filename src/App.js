35a3c884040230911d40fc366f1dc98a2f1e1545
Alberto Oteiza <alberto@almacenguru.com>
2021-12-12 21:33:23 -0300

    modifying material ui default primary palette colors, applied to the whole project

--- src/App.js
+++ src/App.js
@@ -9,21 +9,38 @@ import { Login } from './pages/login/Login'
 import { Home } from './pages/home/Home' 
 import { Profile } from './pages/profile/Profile';
 import { RedirectPage } from './Redirect';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#E54B4B',
+    },
+    secondary: {
+      main: '#FF9585',
+    },
+  },
+  typography: {
+    fontFamily: `"Rubik", "Roboto", "Helvetica", sans-serif`,
+    fontSize: 14,
+   }
+});
 
 function App() {
   return (
-  <Router>
-    <Routes>
-      <Route exact path='/' element={<Login />} />
-      <Route exact path='/home' element={<Home />} />
-      <Route path='/profile'  >
-        <Route path='' element={<Profile />}/>
-        <Route path=':userId' element={<Profile />}/>
-      </Route>
-      <Route exact path='*' element={<RedirectPage />} />
-    </Routes>
-  </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Login />} />
+          <Route exact path='/home' element={<Home />} />
+          <Route path='/profile'  >
+            <Route path='' element={<Profile />}/>
+            <Route path=':userId' element={<Profile />}/>
+          </Route>
+          <Route exact path='*' element={<RedirectPage />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Button, Grid } from '@mui/material';
 import './Home.css'
 
 export function Home() {
 
   return (
    <div className='home-page-container'>
-     
+     <div className='basic-page-template'>
+      <Grid container>
+        <Grid item xs={12}>
+          <div className='home-page-header'>
+            <div className='title'>Registered Sheets</div>
+            <Button variant="contained">Add Sheet</Button>
+          </div>
+        </Grid>
+      </Grid>
+     </div>
    </div>
   )
 }
